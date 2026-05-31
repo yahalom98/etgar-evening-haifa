70f90b9a37e789a09c31cfede1d84b4087609b5f
yahalom98 <twixy100@gmail.com>
2026-05-31 18:56:37 +0300

    updated

--- x/src/App.jsx
+++ x/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import AnotherForm from './components/AnotherForm'
+import { useState, useEffect } from 'react'
 
 
 export default function App() {
+  let [apiData, setApiData] = useState([]);
 
+
+  async function loadData() {
+    let server = await fetch('https://fakestoreapi.com/products');
+    let data = await server.json();
+    setApiData(data);
+  }
   return (
     <div>
-    <AnotherForm/>
+
     </div>
   )
 }
